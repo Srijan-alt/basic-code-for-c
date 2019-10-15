8c1c366adc358d4e0762ba4c6d85f2a41b0044b3
Srijan-alt <55505220+Srijan-alt@users.noreply.github.com>
2019-10-15 18:33:39 +0530

    Create vowel.c
    This code is to check for vowel

---
+++ vowel.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    char ko;
+    printf("enter a char\n");
+    scanf("%c",&ko);
+    if(ko=='a')
+    {
+    printf("vowel a");
+    }
+    else if(ko=='e')
+    {
+    printf("vowel e");
+    }
+    else if(ko=='i')
+    {
+    printf("vowel i");
+    }
+    else if(ko=='o')
+    {
+    printf("vowel o");
+    }
+    else if(ko=='u')
+    {
+    printf("vowel u");
+    }
+    else
+    {
+        printf("not vowel");
+    }
+}
