96ed39824ea1ccab012e0e7f3702847bf295d6b8
Srijan-alt <55505220+Srijan-alt@users.noreply.github.com>
2019-10-21 17:47:09 +0530

    Create length.c
    This code tells the no. of letter in a user given word

---
+++ length.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//#include<string.h>
+void mian()
+{
+    int i;
+    char a[20];
+    printf("enter");
+    gets(a);
+    for(i=0;a[i];i++)
+    {
+    }
+    printf("%d",i);
+}
