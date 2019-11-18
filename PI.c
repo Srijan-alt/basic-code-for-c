3eb30cde216b1655933e8faa775bc73dd3162013
Srijan-alt <55505220+Srijan-alt@users.noreply.github.com>
2019-11-18 18:20:00 +0530

    Create PI.c
    This code is a example of sizeof code

---
+++ PI.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define PI 3.14
+void main()
+{
+    const int a=13;
+    printf("the int you entered %d\n",a);
+    printf("the value of PI is %f\n",PI+20);
+    printf("the size of the int you entered is =%d\n",sizeof(a));
+
+}
